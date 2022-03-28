b5d87d088e2829b6512e559aab463572a8ee7a65
易庞宙 <yipangzhou@xa.com>
2022-03-28 11:04:55 +0800

    commit permission code

--- app/src/main/java/com/killetom/jetpack/support/MainActivity.kt
+++ app/src/main/java/com/killetom/jetpack/support/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.killetom.jetpack.support
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.killetom.jetpack.permission.KTSupportPermission
+import com.killetom.jetpack.support.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val ktSupportPermission = KTSupportPermission()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        ktSupportPermission.attach(this)
+
+        binding.getMultipePermission.setOnClickListener {
+
+            val permissions = arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+
+            ktSupportPermission
+                .requestPermission(permissions) {
+
+                    //理成功后逻辑
+                    okAction = {}
+                    //处理失败后逻辑
+                    failAction = {}
+
+                }
+        }
+
+        binding.getSinglePermission.setOnClickListener {
+
+            ktSupportPermission
+                .requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) {
+
+                    //理成功后逻辑
+                    okAction = {}
+                    //处理失败后逻辑
+                    failAction = {}
+
+                }
+        }
+
     }
 }
